b2e84d9b3464b8e14e5f44129e93973c8d8ff587
omkar861856 <omkar861856@gmail.com>
2024-08-17 17:04:44 +0530

    todo api

--- index.js
+++ index.js
@@ -1,31 +1,80 @@
-import express from 'express'
-import 'dotenv/config'
-import { main } from './db.js';
+import express from "express";
+import "dotenv/config";
+import { main } from "./db.js";
+import { ObjectId } from "mongodb";
 
-const app = express();  
+const app = express();
+
+// middleware to parse incoming requests
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
 const db = await main();
-const collection = db.collection('todos');
+const collection = db.collection("todos");
 
+app.get("/", function (req, res) {
+  res.send("Hello World of Todos");
+});
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.post("/todo", async (req, res) => {
+  try {
+    const todo = req.body;
+    const insertResult = await collection.insertOne(todo);
+    console.log("Inserted documents =>", insertResult);
+    res.status(201).send({ msg: "Inserted todo successfully", insertResult });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
 
-app.post('/', (req, res)=>{
+app.get("/todos", async (req, res) => {
+  try {
+    const findResult = await collection.find({}).toArray();
+    console.log("Found documents =>", findResult);
+    res.status(200).send(findResult);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
 
-})
+// url prams
 
-app.put('/', (req, res)=>{
-  
-})
+// query params
 
-app.delete('/', (req, res)=>{
-  
-})
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const _id = new ObjectId(id);
 
-app.listen(PORT, ()=>{
-  console.log("Server running on port 3000")
-})
+    const findResult = await collection.findOne({ _id });
+    console.log("Found documents =>", findResult);
+    res.status(200).send(findResult);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const _id = new ObjectId(id);
+
+    const findResult = await collection.deleteOne({ _id });
+    if (findResult.deletedCount === 0) {
+      res.send({ msg: "Todo doesnt exist" });
+    }
+    console.log("Found documents =>", findResult);
+    res.status(200).send({ findResult, msg: "Todo deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port 3000");
+});
